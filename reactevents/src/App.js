22b1e71cfe7a1634868d9eacef4a272b095fd0ff
Marcin Polonis <marcin.polonis@o2.pl>
2020-07-27 15:09:19 +0200

    Continue makeing training

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -5,7 +5,9 @@ export default class App extends Component {
     super(props);
 
     this.state = {
-      message: "Gotowy"
+      message: "Gotowy",
+      counter: 0,
+      theme: "secondary"
     }
   }
 
@@ -13,18 +15,33 @@ export default class App extends Component {
     this.setState({ message: `kliknięty (${event.type})`})
   }
 
-  handleEvent = () => {
-    console.log("Wywołano metodę handleEvent")
+  handleEvent = (event, newTheme) => {
+    event.persist();
+    this.setState({counter: this.state.counter + 1,
+        theme: newTheme
+    },
+        () => this.setState({message: `${event.type}: ${this.state.counter}`}));
+  }
+
+  toggleCheckBox = (event) => {
+    if(this.state.counter === 0) {
+      event.preventDefault();
+    }
   }
 
   render() {
     return (
       <div className="m-2">
-        <div className="h4 bg-primary text-white text-center p-2">
+        <div className="form-check">
+          <input className="form-check-input" type="checkbox" onClick={this.toggleCheckBox} />
+          <label>To jest pole wyboru</label>
+        </div>
+        <div className={`h4 bg-${this.state.theme} text-white text-center p-2}`}>
           {this.state.message}
         </div>
         <div className="text-center">
-          <button onClick={this.handleClick} className="btn btn-primary">Kliknij mnie!</button>
+          <button onClick={(e) => this.handleEvent(e, "primary")} className="btn btn-primary">normalny</button>
+          <button onClick={(e) => this.handleEvent(e, "danger")} className="btn btn-danger">uwaga!</button>
         </div>
       </div>
     )
